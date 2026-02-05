fd9f2ca5264591fe4fa791353bb13b831af26bd1
nathankipchumba <nathankipchumba97@gmail.com>
2026-02-05 18:02:01 +0300

    added more pages and a little js

---
+++ findit/js/index.js
@@ -0,0 +1,6 @@
+let account = document.querySelector(".account");
+let accountInfo = document.querySelector(".account-info");
+
+account.addEventListener("click", () => {
+  accountInfo.classList.toggle("info");
+});
